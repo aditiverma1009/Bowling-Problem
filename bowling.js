c750ca060143a4e0b79c574447199e862369e66b
aditiverma1009 <aditiverma1009@gmail.com>
2018-01-23 11:50:26 +0530

    Cannot solve all spare test case without changing approach. Using seperate function to control finding the type of frame

--- bowling.js
+++ bowling.js
@@ -1,9 +1,18 @@
 module.exports = function scoringBowling(scoreArr) {
   let totalScore = 0;
-  scoreArr.forEach((value) => {
-    totalScore += value;
-  });
+  if (scoreArr.length === 12) {
+    for (let i = 0; i < 20; i += 1) {
+      checkType(scoreArr[i],scoreArr[i+1];
+    }
+  } else {
+    scoreArr.forEach((value) => {
+      totalScore += value;
+    });
+  }
+
   // console.log(typeof (totalScore));
   // console.log(totalScore);
   return totalScore;
 };
+
+const checkType = (throw1,throw2)=>let sum=throw1+throw2;
