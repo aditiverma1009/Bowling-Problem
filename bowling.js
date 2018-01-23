2f1426c4331dd1c8c5c1057410e2f90d67f61f88
aditiverma1009 <aditiverma1009@gmail.com>
2018-01-23 12:35:11 +0530

    Working for 9 frames

--- bowling.js
+++ bowling.js
@@ -12,30 +12,34 @@ module.exports = function scoringBowling(scoreArr) {
   let totalScore = 0;
   let frame = 0;
   let i = 0;
-  while (frame < 9) {
-    const throw1 = scoreArr[i];
-    const throw2 = scoreArr[i + 1];
-    const throw3 = scoreArr[i + 2];
-    console.log(`throw1 : ${throw1}`);
-    console.log(`throw2 : ${throw2}`);
-    console.log(`throw3 : ${throw3}`);
+  if (scoreArr.length === 20) {
+    while (frame < 9) {
+      const throw1 = scoreArr[i];
+      const throw2 = scoreArr[i + 1];
+      const throw3 = scoreArr[i + 2];
+      console.log(`throw1 : ${throw1}`);
+      console.log(`throw2 : ${throw2}`);
+      console.log(`throw3 : ${throw3}`);
 
-    const typeOfFrame = checkType(throw1, throw2);
-    console.log(`result of typeofframe : ${typeOfFrame}`);
-    if (typeOfFrame === 1) { // strike case
-      i += 1;
-      totalScore += throw1;
-    } else if (typeOfFrame === 2) { // spare case
-      i += 2;
-      totalScore += (throw1 + throw2 + throw3);
-    } else if (typeOfFrame === 3) { // open frame case
-      i += 3;
-      totalScore += (throw1 + throw2);
+      const typeOfFrame = checkType(throw1, throw2);
+      console.log(`result of typeofframe : ${typeOfFrame}`);
+      if (typeOfFrame === 1) { // strike case
+        i += 1;
+        totalScore += throw1;
+      } else if (typeOfFrame === 2) { // spare case
+        i += 2;
+        totalScore += (throw1 + throw2 + throw3);
+      } else if (typeOfFrame === 3) { // open frame case
+        i += 3;
+        totalScore += (throw1 + throw2);
+      }
+      frame += 1;
+      console.log(`no of frame : ${frame}`);
+      console.log(`totalscore : ${totalScore} ----------`);
     }
-    frame += 1;
-    console.log(`no of frame : ${frame}`);
-    console.log(`totalscore : ${totalScore} ----------`);
-  }
+  } else {
 
+
+  }
   return totalScore;
 };
