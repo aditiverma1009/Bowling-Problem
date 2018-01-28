79358df1113bfb84231ae069aa7be35c1becf1f5
aditiverma1009 <aditiverma1009@gmail.com>
2018-01-28 20:24:08 +0530

    including gitignore

--- bowling.test.js
+++ bowling.test.js
@@ -11,7 +11,7 @@ test('Testing for all 0 strikes and hence the result must be 0', () => {
 //   expect(calculatedScore(inputAllOpenFrame)).toBe(41);
 // });
 //
-// // we test for 20 throws first
+// we test for 20 throws first
 //
 // const inputTest1 = [8, 2, 3, 7, 4, 6, 1, 9, 9, 1, 3, 7, 2, 8, 8, 2, 1, 9, 1, 8];
 // test('Testing for all spare frames but 10th frame be an open frame hence the result must be ', () => {
